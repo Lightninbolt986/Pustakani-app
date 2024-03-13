ee4e0667905ee3496152c9ddd33c8a21da657af0
-LilNug- <dk15022007@gmail.com>
2024-03-13 12:49:32 +0530

    Added styling to landing and profile pages

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,24 +1,31 @@
 import React from "react";
+import "./LandingPage.css";
 
 function LandingPage() {
   return (
-    <div>
+    <div className="button-box">
       <div>
         <button
+          className="donate-button"
           onClick={function e() {
             window.location.href = "/#profile";
           }}
         >
           Donate Books
+          <br />
+          पुस्तकें दान
         </button>
       </div>
       <div>
         <button
+          className="receive-button"
           onClick={function e() {
             window.location.href = "/#receive";
           }}
         >
           Receive Books
+          <br />
+          पुस्तकें प्राप्त
         </button>
       </div>
     </div>
