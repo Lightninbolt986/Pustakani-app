5390722650dceaec1eadd807c6b34e950addecb4
Lightninbolt986 <68819260+Lightninbolt986@users.noreply.github.com>
2024-03-22 12:17:40 +0530

    receive page functionality done

--- src/ReceivePage/ReceivePage.js
+++ src/ReceivePage/ReceivePage.js
@@ -1,10 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import app from "../firestore";
+import BookBox from "./bookBox";
+const subjects = [
+  "Mathematics",
+  "Science",
+  "Social Science",
+  "Physics",
+  "Accountancy",
+  "Sanskrit",
+  "Hindi",
+  "English",
+  "Biology",
+  "History",
+  "Geography",
+  "Psychology",
+  "Sociology",
+  "Chemistry",
+  "Political Science",
+  "Economics",
+  "Business Studies",
+  "Home Science",
+  "Urdu",
+  "Fine Art",
+  "Computer Science",
+];
 
 function ReceivePage() {
+  let [books, setBooks] = useState([]);
+  const searchBooks = (e) => {
+    e.preventDefault();
+    const classSelect = document.getElementById("ClassSelect").value;
+    const subjectSelect = document.getElementById("SubjectSelect").value;
+    const db = getFirestore(app);
+    getDocs(collection(db, "users")).then((booksRef) => {
+      booksRef = booksRef.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      books = [];
+      setBooks(books);
+      for (let i = 0; i < booksRef.length; i++) {
+        const bok = booksRef[i].books;
+        for (let j = 0; j < bok.length; j++) {
+          const element2 = bok[j];
+          if (
+            element2.class === classSelect &&
+            element2.subject === subjectSelect
+          ) {
+            books = [
+              ...books,
+              {
+                book: element2,
+                id: booksRef[i].id,
+                phone: booksRef[i].phone,
+                address: booksRef[i].address,
+              },
+            ];
+          }
+        }
+      }
+      setBooks(books);
+    });
+  };
   return (
     <div>
       <div>
-        <button>Receive Books</button>
+        <form onSubmit={searchBooks}>
+          {" "}
+          <label htmlFor="ClassSelect">Class:</label>
+          <select className="form-control" id="ClassSelect">
+            {[...Array(12)].map((e, i) => (
+              <option key={i + 1} value={i + 1}>
+                {i + 1}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="SubjectSelect">Subject:</label>
+          <select className="form-control" id="SubjectSelect">
+            {subjects.map((e, i) => (
+              <option key={e} value={e}>
+                {e}
+              </option>
+            ))}
+          </select>{" "}
+          <Button className="btn btn-primary" type="submit">
+            Submit form
+          </Button>
+        </form>
+      </div>
+      <div>
+        {" "}
+        {books.map((element) => {
+          return <BookBox {...element} />;
+        })}
       </div>
     </div>
   );
---
+++ src/ReceivePage/bookBox.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+
+const BookBox = ({ book, id, phone, address }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <div className="book-box">
+        <h3>{book.title}</h3>
+        <p>Class: {book.class}</p>
+        <p>Subject: {book.subject}</p>
+        <button className="btn btn-primary" onClick={handleShow}>
+          View
+        </button>
+      </div>
+
+      {show && (
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Book Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h3>{book.title}</h3>
+            <p>Class: {book.class}</p>
+            <p>Subject: {book.subject}</p>
+            <p>Donor phone number: {phone}</p>
+            <p>
+              Address:{" "}
+              {` ${address.address}, ${address.district}, ${address.state}`}
+            </p>
+          </Modal.Body>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default BookBox;
